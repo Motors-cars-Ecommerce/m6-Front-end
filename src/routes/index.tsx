d13f721fc7cd7d7cf77e96ecf22e25f6125c2066
Guilherme Wallace <gpeixoto938@gmail.com>
2023-04-14 02:01:31 -0300

    feat: product card referring to the information page of the car created

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,13 @@ import { Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard/dashboard";
 import HomeCarCard from "../components/HomeCarCard/HomeCarCard";
 import NavBar from "../components/NavBar/NavBar";
+import CardProduct from "../components/CardProduct/CardProduct";
 
 export const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      {/* <Route path="/" element={<Dashboard />} /> */}
+      <Route path="/" element={<CardProduct />} />
     </Routes>
   );
 };
