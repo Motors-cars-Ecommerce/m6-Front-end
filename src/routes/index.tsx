1ca38bc513702176457eafb22f3229ed071c30d3
Jean Michel Biz <jeanmbiz@hotmail.com>
2023-04-14 10:32:35 -0300

    mobile filter navbar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ import NavBar from "../components/NavBar/NavBar";
 export const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<NavBar />} />
     </Routes>
   );
 };
