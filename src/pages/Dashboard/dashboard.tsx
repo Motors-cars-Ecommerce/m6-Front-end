807af786405d4e25917bf244dd23adfc531d3b56
Jean Michel Biz <jeanmbiz@hotmail.com>
2023-04-19 09:21:57 -0300

    dashboard file

--- src/pages/Dashboard/dashboard.tsx
+++ src/pages/Dashboard/dashboard.tsx
@@ -15,7 +15,7 @@ const Dashboard = () => {
       <DashboardMainStyled>
         <section>
           <NavBarDesktop />
-          <NavBarMobile/>
+          {/* <NavBarMobile/> */}
             <ul className="container-card">
               <HomeCarCard />
               <HomeCarCard />
