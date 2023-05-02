4dfb9b1341793e4368867f00f72dac09f68d5623
Jean Michel Biz <jeanmbiz@hotmail.com>
2023-05-02 14:53:08 -0300

    implemented logic to open and close modals edit user

--- src/context/DashboardContext.tsx
+++ src/context/DashboardContext.tsx
@@ -4,7 +4,11 @@ import { iChildrenProps } from './salleContext'
 
 interface iDashboardContext{
     filterMobile: boolean,
-    setFilterMobile: React.Dispatch<React.SetStateAction<boolean>>
+    setFilterMobile: React.Dispatch<React.SetStateAction<boolean>>,
+    modalEditProfile: boolean,
+    setModalEditProfile: React.Dispatch<React.SetStateAction<boolean>>,
+    modalEditAddress: boolean,
+    setModalEditAddress: React.Dispatch<React.SetStateAction<boolean>>,
 }
 
 export const DashboardContext = createContext({} as iDashboardContext)
@@ -15,9 +19,12 @@ const DashboardProvider = ({children}:iChildrenProps) => {
 
     const [filterMobile, setFilterMobile] = useState<boolean>(false)
 
+    const [ modalEditProfile, setModalEditProfile ] = useState<boolean>(false)
+    const [ modalEditAddress, setModalEditAddress ] = useState<boolean>(false)
+
 
   return (
-    <DashboardContext.Provider value={{setFilterMobile, filterMobile}}>
+    <DashboardContext.Provider value={{setFilterMobile, filterMobile, modalEditProfile, setModalEditProfile, modalEditAddress, setModalEditAddress}}>
         {children}
     </DashboardContext.Provider>
   )
